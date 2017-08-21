2a16de50c916d9f4356afb4d02c62847999702af
Yui Ezic <mishan221199@gmail.com>
2017-08-21 19:37:00 +0300

    Create class maid and upgrade handle_text function in main.py.

---
+++ maid.py
@@ -0,0 +1,7 @@
+class Maid():
+    def __init__(self):
+        self.name = "Служаночка"
+
+    def get_name(self):
+        return self.name
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import telebot
 import constants
+import maid
+
+servant = maid.Maid()
 
 bot = telebot.TeleBot(constants.token)
 
@@ -9,6 +12,8 @@ def handle_text(message):
         bot.send_message(message.from_user.id, "Здравствуй хозяин.")
     elif message.text.lower() in "как дела?":
         bot.send_message(message.from_user.id, "Отлично, спасибо что интерисуешься :3")
+    elif message.text.lower() in ("как тебя зовут?"):
+        bot.send_message(message.from_user.id, "Меня зовут " + servant.get_name())
     else:
         bot.send_message(message.from_user.id, "Простите, я не понимаю что вы хотите :с")
 
